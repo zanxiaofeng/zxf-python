173dd899485cec0121ab7f0af6fef5e99a480afb
Davis ZAN <zanxiaofeng@163.com>
2024-04-07 01:27:36 -0500

    update

---
+++ tcp-client.py
@@ -0,0 +1,16 @@
+import socket
+
+target_host = "localhost"
+target_port = 8080
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host, target_port));
+
+client.send(b"GET / HTTP/1.1\r\nHost: www.163.com\r\n\r\n");
+
+response = client.recv(4096)
+
+print(response.decode())
+
+client.close()
